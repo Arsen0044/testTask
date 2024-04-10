4cd0c81adcbf13444251dcf40de910fa16fbbcad
Arsen Mydzyn <arsik04mydzyn@gmail.com>
2024-04-10 14:22:57 +0300

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://www.saucedemo.com',
+    baseUrl: 'https://www.saucedemo.com/',
     defaultCommandTimeout: 6000
   },
 });
--- cypress/e2e/provideMainFlow.cy.js
+++ cypress/e2e/provideMainFlow.cy.js
@@ -4,6 +4,7 @@ import mainFlowPage from "../pageObjects/mainFlowPage";
 describe("Adding items to Card", () => {
     it('flow', () => {
         cy.login()
+        cy.visit('/')
         mainFlowPage.provideFullFlow();
     })
 })
